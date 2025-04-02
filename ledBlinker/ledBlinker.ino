09499d05162f578aaea7f79b678298c09168f0d4
Ray-is <rayneralla@icloud.com>
2025-04-02 12:50:05 -0400

    Added comments to ledBlinker

--- ledBlinker/ledBlinker.ino
+++ ledBlinker/ledBlinker.ino
@@ -1,14 +1,20 @@
 #define LED_PIN 13
 
-void setup() {
-  pinMode(LED_PIN, OUTPUT);
+void setup() { // setup runs once on startup
+
+  pinMode(LED_PIN, OUTPUT); // set pin connected to the LED as output
+  Serial.begin(9600);       // start USB communication, so we can print
+
 }
 
-void loop() {
+void loop() { // loop runs over and over again
   
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  delay(1000);
+  digitalWrite(13, HIGH);   // turn the LED on
+  Serial.println("LED on"); // print that it's on
+  delay(1000);              // wait 1 second
+
+  digitalWrite(13, LOW);     // turn the LED off
+  Serial.println("LED off"); // print that it's off
+  delay(1000);               // wait 1 second
 
 }
